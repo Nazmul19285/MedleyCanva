811ac070d67224952b2bbf9d3522aa246b666c8a
Nazmul19285 <nazmulhossain19285@gmail.com>
2023-06-24 12:50:30 +0600

    video testing

--- src/components/tavels/Travels.js
+++ src/components/tavels/Travels.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import icon from '../../e1.png';
 
-const Travels = (props) => {
-    // const responsive = {
-    //     desktop: {
-    //         breakpoint: { max: 3000, min: 1024 },
-    //         items: 3,
-    //         slidesToSlide: 3 // optional, default to 1.
-    //     },
-    //     tablet: {
-    //         breakpoint: { max: 1024, min: 464 },
-    //         items: 2,
-    //         slidesToSlide: 2 // optional, default to 1.
-    //     },
-    //     mobile: {
-    //         breakpoint: { max: 464, min: 0 },
-    //         items: 1,
-    //         slidesToSlide: 1 // optional, default to 1.
-    //     }
-    // };
+const Travels = () => {
+
     return (
         <div>
-            <div className="carousel rounded-box flex">
+            <div className="p-16 bg-gray-500">
+                <iframe
+                    src="https://player.gotipath.com/stream/a699eb89-6100-4a23-bdb6-1049905eeb61/default/1e9e73bd-16a3-4ee8-88da-506321ebc6f4?autoplay=true&preload=true&loop=true&muted=true"
+                    loading="lazy"
+                    className="w-full h-full"
+                    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+                    allowfullscreen="true">
+                </iframe>
+            </div>
+
+
+            <div className='flex'>
                 <div className="carousel-item">
                     <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
                 </div>
@@ -45,29 +41,6 @@ const Travels = (props) => {
                     <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
                 </div>
             </div>
-            {/* <Carousel
-                swipeable={false}
-                draggable={false}
-                showDots={true}
-                responsive={responsive}
-                ssr={true} // means to render carousel on server-side.
-                infinite={true}
-                autoPlay={this.props.deviceType !== "mobile" ? true : false}
-                autoPlaySpeed={1000}
-                keyBoardControl={true}
-                customTransition="all .5"
-                transitionDuration={500}
-                containerClass="carousel-container"
-                removeArrowOnDeviceType={["tablet", "mobile"]}
-                deviceType={this.props.deviceType}
-                dotListClass="custom-dot-list-style"
-                itemClass="carousel-item-padding-40-px"
-            >
-                <div>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
-            </Carousel> */}
         </div>
     );
 };
